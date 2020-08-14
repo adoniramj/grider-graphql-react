c5a4aa57d2f2207f0d186fc8c7249892623735b5
adoniramj <adoniramjvargas@gmail.com>
2020-08-14 14:25:53 -0400

    Create schemas.
    Add json-server as a dev dependency.

--- schema/schema.js
+++ schema/schema.js
@@ -1,30 +1,49 @@
 const graphql = require('graphql');
+const axios = require('axios');
+
 
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql
 
 
+
 const UserType = new GraphQLObjectType({
-  name : 'User',
-  fields : {
-    id: { type: GraphQLString },
-    firstName: { type: GraphQLString },
-    age: { type: graphql.GraphQLInt}
+  name: 'User',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    age: {
+      type: graphql.GraphQLInt
+    }
   }
 })
 
-const RootQuery = new GraphQLObjectType ({
-  name : 'RootQueryType',
-  fields : {
-    user : {
-      type : UserType,
-      args : { id: { type : GraphQLString}},
-      resolve(parentValue, args){
-        
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data)
       }
     }
   }
+})
+
+module.exports = new GraphQLSchema({
+  query : RootQuery
 })
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
 
 
 
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
+  schema,
   graphiql: true 
 }))
 
-app.listen(4000, () => {
-  console.log('Listening on 4000');
-})
+app.listen(4000)
